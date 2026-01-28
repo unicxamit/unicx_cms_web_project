6b36898893de4419b450b31c05f68c8f3ae8ed2a
unicxamit <amit.unicx@gmail.com>
2026-01-28 15:34:57 +0530

    Delete build directory

--- build/assets/js/emp-sidebar.js
+++
@@ -1,3 +0,0 @@
-jQuery('#jobMenuId1, #jobMenuId2, #msgMenuId1, #msgMenuId2').parent().parent(".sub-menu, .mega-menu").slideUp(500, function(){
-    jQuery(this).parent().removeClass('nav-active');
-});
